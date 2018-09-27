e5d98e7105cf8fd4f6f1436ec793c6e25083d525
Dani János <dani.janos@gmail.com>
2018-09-27 17:23:44 +0200

    Add face_detector.py

---
+++ face_detector.py
@@ -0,0 +1,55 @@
+import cv2
+import glob
+
+faceDet = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+faceDet_two = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+faceDet_three = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+faceDet_four = cv2.CascadeClassifier("haarcascade_frontalface_alt_tree.xml")
+# Define emotions:
+emotions = ["neutral", "anger", "contempt", "disgust", "fear", "happy", "sadness", "surprise"]
+
+
+def detect_faces(emotion):
+    # Get list of all images with emotion:
+    files = glob.glob("sorted_set\\%s\\*" %emotion)
+    filenumber = 0
+    for f in files:
+        # Open image:
+        frame = cv2.imread(f)
+        # Convert image to gray scale:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Detect face using 4 different classifiers
+        face = faceDet.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=10, minSize=(5, 5), flags=cv2.CASCADE_SCALE_IMAGE)
+        face_two = faceDet_two.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=10, minSize=(5, 5), flags=cv2.CASCADE_SCALE_IMAGE)
+        face_three = faceDet_three.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=10, minSize=(5, 5), flags=cv2.CASCADE_SCALE_IMAGE)
+        face_four = faceDet_four.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=10, minSize=(5, 5), flags=cv2.CASCADE_SCALE_IMAGE)
+        # Go over detected faces, stop at first detected face, return empty if no face:
+        if len(face) == 1:
+            facefeatures = face
+        elif len(face_two) == 1:
+            facefeatures = face_two
+        elif len(face_three) == 1:
+            facefeatures = face_three
+        elif len(face_four) == 1:
+            facefeatures = face_four
+        else:
+            facefeatures = ""
+        # Cut and save face:
+        for (x, y, w, h) in facefeatures: # get coordinates and size of rectangle containing face
+            print("face found in file: %s") % f
+            # Cut the frame to size:
+            gray = gray[y:y+h, x:x+w]
+            try:
+                # Resize face so all images have same size:
+                out = cv2.resize(gray, (350, 350))
+                # Write image:
+                cv2.imwrite("dataset\\%s\\%s.jpg" %(emotion, filenumber), out)
+            except:
+                # If error, pass file
+                pass
+        # Increment image number:
+        filenumber += 1
+
+
+for emotion in emotions:
+    detect_faces(emotion)
