7a045f56322c00a797852d6de1f314ba7a0d794a
Dani János <dani.janos@gmail.com>
2018-12-01 15:23:48 +0100

    Added the first convolution layer

--- cnn_keras_solution/facial_expression_recognition_with_keras_and_ferdb.py
+++ cnn_keras_solution/facial_expression_recognition_with_keras_and_ferdb.py
@@ -1,6 +1,9 @@
 import numpy as np
 import tensorflow as tf
 
+number_of_emotion_classes = 7
+image_dimension = 48
+
 with open('./data/fer2013.csv') as fer:
     expression_data = fer.readlines()
 
@@ -18,7 +21,7 @@ for i in range(1, data_instances):
 
         # one-hot encoding of the data
         # Converts a class vector (integers) to binary class matrix for use with categorical_crossentropy
-        emotion = tf.keras.utils.to_categorical(emotion, num_classes=7)
+        emotion = tf.keras.utils.to_categorical(emotion, num_classes=number_of_emotion_classes)
 
         if 'Training' in usage:
             train_x.append(pixels)
@@ -30,3 +33,9 @@ for i in range(1, data_instances):
         print('', end='')
 
 model = tf.keras.models.Sequential()
+
+# The CNN structure:
+
+# The first convolution layer:
+model.add(tf.keras.layers.Conv2D(64, (5, 5), activation=tf.nn.relu, input_shape=(image_dimension, image_dimension, 1)))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(5, 5), strides=(2, 2)))
