698507072c069cc189ec81de120828509a472746
Dani János <dani.janos@gmail.com>
2018-12-04 23:42:22 +0100

    Save the detected face to a variable

--- FER_from_stream_data/FER_from_steam.py
+++ FER_from_stream_data/FER_from_steam.py
@@ -3,22 +3,28 @@ import cv2
 face_detector = cv2.CascadeClassifier('../venv/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml')
 
 capture_stream = cv2.VideoCapture(0)
+capture_stream.set(3, 640)  # width
+capture_stream.set(4, 480)  # height
 
 while True:
-    ret, img = capture_stream.read()
+    ret, frame = capture_stream.read()
 
-    img_in_grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    image_in_grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = face_detector.detectMultiScale(
-        img_in_grayscale,
+        image_in_grayscale,
         scaleFactor=1.1,
         minNeighbors=10,
         flags=cv2.CASCADE_SCALE_IMAGE,
         minSize=(3, 3))
 
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        cv2.imshow('img', img)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)  # Rectangle around the face
+        cv2.imshow('Stream from the camera', frame)
+
+        detected_face = frame[y:y + h, x:x + w]  # crop detected face
+        detected_face = cv2.cvtColor(detected_face, cv2.COLOR_BGR2GRAY)  # transform to gray scale
+        detected_face = cv2.resize(detected_face, (48, 48))  # resize to 48x48
 
     # press q to quit
     if cv2.waitKey(1) & 0xFF == ord('q'):
